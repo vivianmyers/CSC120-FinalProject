31a4494122e899ae0665395d7f7754569e41d056
Evelyn Ray <eray@smith.edu>
2024-11-24 15:54:17 -0500

    game main work

--- GameMain.java
+++ GameMain.java
@@ -2,9 +2,31 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class GameMain{
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
+         // This is a "flag" to let us know when the loop should end
+         boolean stillPlaying = true;
+
+         // We'll use this to get input from the user.
+         Scanner userInput = new Scanner(System.in);
+ 
+         // Storage for user's responses
+         String userResponse = "";
+
+        System.out.println("You are a sheep herder with 10 sheep. You must find your way back to the barn with at least 7 sheep.");
+        
+        do {
+
+
+            
+        } while (stillPlaying);
+
+
+        userInput.close();
+
     }
 }   
+
+  
 
