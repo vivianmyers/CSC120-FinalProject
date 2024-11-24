cab34809b60b31da6d428e3fa1f43a4bfdd0f6af
Vivian Myers <vmyers@smith.edu>
2024-11-24 15:33:54 -0500

    created GameMain class

---
+++ GameMain.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class GameMain{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+    }
+}   
+
