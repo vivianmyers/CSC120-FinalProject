e6dcd7eb2a446b0ba95e53ac29b735383b727c41
Evelyn Ray <eray@smith.edu>
2024-11-24 15:31:11 -0500

    Place class

--- Place.java
+++ Place.java
@@ -8,4 +8,16 @@ public class Place{
     ArrayList<Item> items;
     NPC npc;
 
+    public Place(String name, String description, int x, int y, ArrayList<Item> items, NPC npc){
+        this.name = name;
+        this.description = description;
+        this.placeX = x;
+        this.placeY = y;
+        this.items = items;
+        this.npc = npc;
+    }
+
+    public String describe(){
+        return this.description;
+    }
 }
